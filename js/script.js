ca2f0e74ce82bdb0d4903b905ea475716de75ce2
dan <daniel.munozdev@gmail.com>
2017-09-10 17:30:59 -0700

    Add javascript to switch image in modal view with keyboard arrows

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const imageAltList = [
   'Picture of a bird'
 ];
 
-
+let currentIndex;
 const galleryView = document.querySelector('.gallery-view');
 const modalView = document.querySelector('.modal-view');
 
@@ -28,6 +28,7 @@ function createImage(src, alt) {
 }
 
 function onImageClick(event) {
+  currentIndex = event.currentTarget.dataset.index;
   const image = createImage(event.currentTarget.src, event.currentTarget.alt);
 
   document.body.classList.add('no-scroll');
@@ -35,6 +36,7 @@ function onImageClick(event) {
   modalView.classList.remove('hidden');
 
   modalView.appendChild(image);
+  document.addEventListener('keydown', nextImage);
 }
 
 function hideModal() {
@@ -48,10 +50,40 @@ function onModalClick() {
   hideModal();
 }
 
+
+function nextImage(event) {
+  if (event.key === 'Escape') {
+    hideModal();
+    return;
+  }
+
+  if (event.key !== 'ArrowLeft' && event.key !== 'ArrowRight') {
+    return;
+  }
+
+  let nextIndex = currentIndex;
+  if (event.key === 'ArrowLeft') {
+    nextIndex--;
+  } else {
+    nextIndex++;
+  }
+
+  if (nextIndex < 0 || nextIndex == imageSourceList.length) {
+    return;
+  }
+  const imageSrc = imageSourceList[nextIndex];
+  const imageAlt = imageAltList[nextIndex];
+  modalView.innerHTML = '';
+  const image = createImage(imageSrc, imageAlt);
+  modalView.appendChild(image);
+  currentIndex = nextIndex;
+}
+
 for (let i = 0; i < imageSourceList.length; i++) {
   const imageSrc = imageSourceList[i];
   const imageAlt = imageAltList[i];
   const image = createImage(imageSrc, imageAlt);
+  image.dataset.index = i;
   image.addEventListener('click', onImageClick);
   galleryView.appendChild(image);
 }
